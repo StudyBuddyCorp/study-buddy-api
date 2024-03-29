5662abf7e39644f9e9436975265f1c0a91fa16ef
Papla2016 <papla2016@yandex.ru>
2024-03-29 16:35:42 +0300

    Changed from CollectionModel to RepresentationModel Fixed empty _embedded

--- src/main/java/com/ru/studybuddy/user/UserRepository.java
+++ src/main/java/com/ru/studybuddy/user/UserRepository.java
@@ -4,14 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
-
     Optional<User> getByEmail(String email);
-
-
 
 }
